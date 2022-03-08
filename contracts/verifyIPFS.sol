fb4ef32c0f5724269873209edae9e14481d59f8e
Dr. Sachin Gupta <sachin.gupta@mvn.edu.in>
2022-03-08 09:33:43 +0530

    Update verifyIPFS.sol

--- contracts/verifyIPFS.sol
+++ contracts/verifyIPFS.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.8.0;
 
-/// @title verifyIPFS
-/// @author Martin Lundfall (martin.lundfall@gmail.com)
 library verifyIPFS {
   bytes constant prefix1 = hex"0a";
   bytes constant prefix2 = hex"080212";
